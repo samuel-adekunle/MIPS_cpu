c51a91e715485d97b9764b251278c82a197f885c
tsersiew <tser.siew19@imperial.ac.uk>
2020-11-27 10:51:03 +0000

    PC, Data Mem, Instr Mem, Registers, ShiftLeft2, SignExtend

---
+++ rtl/Registers.v
@@ -0,0 +1,37 @@
+module Registers ( 
+	input logic clk,  
+	input RegWrite,  
+	//5-bit inputs as we have 32 registers in total 
+	input logic [4:0] ReadReg1,  
+	input logic [4:0] ReadReg2,  
+	input logic [4:0] WriteReg,  
+	//WriteData: 32-bit input to be written into a register 
+	input logic [31:0] WriteData; 
+	//32-bit data read from registers selected by ReadReg, WriteReg 
+	output logic [31:0] ReadData1,  
+	output logic [31:0] ReadData2 
+); 
+
+//32 32-bit registers. Initialise register values using regmem.txt 
+logic [15:0] register [0:31]; 
+
+initial begin 
+	$readmemh("regmem.txt", reg_mem, 0, 31); 
+end 
+
+//output ports. 32-bit data read from registers selected by ReadReg, WriteReg 
+assign ReadData1 = register[ReadReg1]; 
+assign ReadData2 = register[ReadReg2]; 
+
+//at rising edge of clk, if RegWrite_en is 1, write WriteData into register selected by WriteReg 
+
+always_ff @(posedge clock) begin 
+	if (RegWrite) begin 
+	register[WriteReg] <= WriteData; 
+	end  
+end  
+
+endmodule 
+
+ 
+ 
