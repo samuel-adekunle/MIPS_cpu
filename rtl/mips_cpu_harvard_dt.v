644495d69846217cfe1129d238e69c690a13650d
reiaxu <59967035+reiaxu@users.noreply.github.com>
2020-12-01 06:23:29 +0000

    in the process of trying dt method?

--- rtl/mips_cpu_harvard_dt.v
+++ rtl/mips_cpu_harvard_dt.v
@@ -21,11 +21,37 @@ module mips_cpu_harvard(
 ); 
 
 	/* Using an enum to define constants */
-    	typedef enum logic[5:0] {
+    	typedef enum logic[31:26] {
         	OPCODE_ADDIU = 6'b001001,
 		OPCODE_RTYPE = 6'b000000,
 		OPCODE_LW = 6'b100011,
-		OPCODE_SW = 6'b101011
+		OPCODE_SW = 6'b101011,
+
+		OPCODE_ANDI = 6'b001100,
+		OPCODE_ORI = 6'b001101,
+		OPCODE_XORI = 6'b001110,
+		OPCODE_BEQ = 6'b000100,
+		OPCODE_BEQZ = 6'b000001,
+		OPCODE_BGTZ = 6'b000111,
+		OPCODE_BLEZ = 6'b000110,
+		OPCODE_BNE = 6'b000101,
+
+		OPCODE_J = 6'b000010,
+		OPCODE_JAL = 6'b000011,
+	
+		OPCODE_LB = 6'b100000,
+		OPCODE_LBU = 6'b100100,
+		OPCODE_LH = 6'b100001,
+		OPCODE_LHU = 6'b100101,
+		OPCODE_LUI = 6'b001111,
+		OPCODE_LWL = 6'b100010,
+		OPCODE_LWR = 6'b100110,
+
+		OPCODE_SB = 6'b101000,
+		OPCODE_SH = 6'b101001,
+
+		OPCODE_SLTI = 6'b001010,
+		OPCODE_SLTIU = 6'b001011,
     	} opcode_t;
 
 	typedef enum logic[5:0] {
@@ -54,8 +80,8 @@ module mips_cpu_harvard(
 
 	// Decide what address to put out on the bus, and whether to write
     	assign instr_address = (state==IF) ? pc : instr_constant;
-    	assign write = state==EXEC_INSTR ? instr_opcode==OPCODE_STO : 0;
-    	assign read = (state==FETCH_INSTR) ? 1 : (instr_opcode==OPCODE_LDA || instr_opcode==OPCODE_ADD  || instr_opcode==OPCODE_SUB );
+    	assign write = state==EXEC_INSTR ? instr_opcode==OPCODE_SW : 0; // Write
+    	assign read = (state==FETCH_INSTR) ? 1 : (instr_opcode==OPCODE_LW || instr_opcode==OPCODE_ADDIU || instr_opcode==OPCODE_ADDU || instr_opcode==OPCODE_AND || instr_opcode==OPCODE_ANDI); // Read
     	assign writedata = acc;
 
 	// Break-down the instruction into fields
