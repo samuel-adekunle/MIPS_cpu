5d809560fbdd5d7aec0ea9cf385f4da93c232c52
tsersiew <tser.siew19@imperial.ac.uk>
2020-12-02 10:45:28 +0000

    included reset in reg file

--- rtl/Registers.v
+++ rtl/Registers.v
@@ -1,6 +1,7 @@
 module Registers ( 
 	input logic clk,  
 	input logic RegWrite,  
+	input logic reset,
 	//5-bit inputs as we have 32 registers in total 
 	input logic [4:0] ReadReg1,  
 	input logic [4:0] ReadReg2,  
@@ -15,6 +16,9 @@ module Registers (
 
 //32 32-bit registers. Initialise register values using regmem.txt 
 logic [31:0] register [0:31]; 
+//for loop 
+int i; 
+
 
 initial begin 
 	$readmemh("regmem.txt", register, 0, 31); 
@@ -28,6 +32,11 @@ assign register_v0 = register[2];
 //at rising edge of clk, if RegWrite_en is 1, write WriteData into register selected by WriteReg 
 
 always_ff @(posedge clk) begin 
+	if (reset) begin
+		for (i = 0; i < 31; i = i + 1) begin
+  			register[i] <= '0;
+		end
+	end
 	if (RegWrite) begin 
 	register[WriteReg] <= WriteData; 
 	end  
