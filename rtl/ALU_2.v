58424e0fed748cec10c2acd779de8ab01758c9bf
coolio982 <wendy.gao@live.co.uk>
2020-12-06 16:47:01 +0000

    added opcode

--- rtl/ALU_2.v
+++ rtl/ALU_2.v
@@ -20,7 +20,7 @@ module ALU_2 (
   logic signed [31:0] temp, signed_rs, signed_rt;
   logic [31:0] signExtend, zeroExtend;
 
-  always @ (functcode, rs_content, rt_content, shamt, immediate)
+  always @ (functcode,opcode, rs_content, rt_content, shamt, immediate)
   begin
 
     // signed value assigment
