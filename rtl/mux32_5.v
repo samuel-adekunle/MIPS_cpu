18d056416ff5d716baa73a00d8ade0227e5003c9
coolio982 <wendy.gao@live.co.uk>
2020-12-12 12:24:11 +0000

    attempting to fix hi lo

--- rtl/mux32_5.v
+++ rtl/mux32_5.v
@@ -2,8 +2,8 @@ module mux32_5 (
     input logic [31:0] InputA,
     input logic [31:0] InputB,
     input logic [31:0] InputC,
-    input logic [31:0] HI_reg, 
-    input logic [31:0] LO_reg, 
+    input logic [31:0] HI_reg,
+    input logic [31:0] LO_reg,
     input logic [2:0] CtlSig,
     output logic [31:0] Output
   );
@@ -16,12 +16,12 @@ module mux32_5 (
       1:
         Output = InputB;
       //FIXME - default case for multiplexer
-      2: 
-	Output = InputC; 
-      3: 
-	Output = HI_reg;
-      4: 
-	Output = LO_reg;  
+      2:
+        Output = InputC;
+      3:
+        Output = HI_reg;
+      4:
+        Output = LO_reg;
     endcase
   end
 endmodule
