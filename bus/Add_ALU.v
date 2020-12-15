760b843572e4b98dff899372dfc57d829802e119
tsersiew <tser.siew19@imperial.ac.uk>
2020-12-15 11:34:09 +0000

    starting bus

---
+++ bus/Add_ALU.v
@@ -0,0 +1,10 @@
+module Add_ALU(
+    input logic[31:0] PCplus4,
+    input logic[31:0] extendImm,
+    output logic[31:0] Add_ALUresult
+  );
+  always_comb
+  begin
+    Add_ALUresult = PCplus4 + extendImm;
+  end
+endmodule
---
+++ bus/instr_mem_1.v
@@ -0,0 +1,29 @@
+module instr_mem_1 (
+
+    input logic [31:0] address,
+    input logic clk,
+    output logic [31:0] instr
+  );
+
+  //instr mem has capacity for 4096 32-bit entries.
+  //initialise the content at each address using a text file containing the instructions.
+
+  // REVIEW logic [31:0] memory [0:4095];
+  logic [31:0] memory[4096];
+
+  initial
+  begin
+    $readmemh("data/instructions_0.txt", memory);
+  end
+
+  //we use byte addressing hence 2 LSB is ignored
+  //combi read path
+  always_comb
+  begin
+    instr = memory[(address-32'hBFC00000)>>2];
+  end
+
+endmodule
+
+
+
