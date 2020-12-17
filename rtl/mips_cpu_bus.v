9a15f953e4ef018e11713d362e2a5c5c85031c36
Samuel Adekunle <me@samuel-adekunle.software>
2020-12-17 13:57:26 +0000

    feat: mips_bus interface
    Signed-off-by: Samuel Adekunle <me@samuel-adekunle.software>

--- rtl/mips_cpu_bus.v
+++ rtl/mips_cpu_bus.v
@@ -1,16 +1,41 @@
-// module mips_cpu_bus(
-//     /* Standard signals */
-//     input logic clk,
-//     input logic reset,
-//     output logic active,
-//     output logic[31:0] register_v0,
+`include "rtl/mips_cpu_harvard.v"
+module mips_cpu_bus(
+    /* Standard signals */
+    input logic clk,
+    input logic reset,
+    output logic active,
+    output logic[31:0] register_v0,
 
-//     /* Avalon memory mapped bus controller (master) */
-//     output logic[31:0] address,
-//     output logic write,
-//     output logic read,
-//     input logic waitrequest,
-//     output logic[31:0] writedata,
-//     output logic[3:0] byteenable,
-//     input logic[31:0] readdata
-// );
+    /* Avalon memory mapped bus controller (master) */
+    output logic[31:0] address,
+    output logic write,
+    output logic read,
+    input logic waitrequest,
+    output logic[31:0] writedata,
+    output logic[3:0] byteenable,
+    input logic[31:0] readdata
+);
+
+logic clk_enable;
+logic[31:0] data_address;
+logic[31:0] instr_address;
+logic[31:0] instr_readdata;
+logic[31:0] data_readdata;
+// TODO - assign byteenable
+
+mips_cpu_harvard cpu (
+  .clk(clk),
+  .reset(reset),
+  .active(active),
+  .register_v0(register_v0),
+  .clk_enable(clk_enable),
+  .instr_address(instr_address),
+  .instr_readdata(instr_readdata),
+  .data_address(data_address),
+  .data_write(write),
+  .data_read(read),
+  .data_writedata(writedata),
+  .data_readdata(readdata)
+);
+
+endmodule
