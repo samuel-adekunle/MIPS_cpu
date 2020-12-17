7492b4879c40246cc4b5911ce96dfbc735734905
Samuel Adekunle <me@samuel-adekunle.software>
2020-12-17 14:31:53 +0000

    restructure files
    Signed-off-by: Samuel Adekunle <me@samuel-adekunle.software>

--- bus/ShiftLeft2.v
+++
@@ -1,8 +0,0 @@
-module ShiftLeft2(
-    input logic [31:0] ShiftIn,
-    output logic [31:0] ShiftOut
-  );
-
-  assign ShiftOut = ShiftIn << 2;
-
-endmodule
--- bus/add4.v
+++
@@ -1,9 +0,0 @@
-module add4 (
-    input logic[31:0] PC,
-    output logic[31:0] PCplus4
-  );
-  always_comb
-  begin
-    PCplus4 = PC + 4;
-  end
-endmodule
